a8b7ac4915d13cd18da912314decfc637483269b
mendezangel <id.angelmendez@gmail.com>
2022-08-17 08:14:24 -0700

    saving work

--- basic-sql/where.sql
+++ basic-sql/where.sql
@@ -26,7 +26,14 @@ _                                             used anywhere in a string to match
 //                                            Only with LIKE and NOT LIKE.                            matches "AND", but not "AN"
 
 
-NOTE: make sure strings have quotes
+NOTE: make sure strings have quotes (single quotes)
+
+Derived Columns: Creating a new column that is a combination of existing columns is known as a derived column. Usually you want to give
+a name to your new column using the AS keyword.
+ex. 
+SELECT id, (standard_amt_usd/total_amt_usd)*100 AS std_percent, total_amt_usd
+FROM orders
+LIMIT 10;
 */
 
 -- query that pulls first 5 rows and all columns from orders table taht have a dollar amount of gloss_amt_usd greate than or equal to 1000.
@@ -40,3 +47,12 @@ SELECT *
 FROM orders
 WHERE total_amt_usd < 500
 LIMIT 10;
+
+-- Filter the accounts table to include the company name, website, and the primary point of contact (primary_poc)
+-- just for the Exxon Mobil company in the accounts table.
+SELECT name, website, primary_poc
+FROM accounts
+WHERE name = 'Exxon Mobil';
+
+-- create a column that divides the standard_amt_usd by the standard_qty to find the unit price for standard paper for each order. Limit the
+-- results to the first 10 orders, and include the id and account_id fields.
